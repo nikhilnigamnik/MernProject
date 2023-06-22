3c6309e2d095e4df1cf211f1bbd3b9ccad49eecb
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-06-22 22:55:09 +0530

    update cancel and success layout

--- Frontend/src/pages/Cancel.jsx
+++ Frontend/src/pages/Cancel.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Cancel = () => {
   return (
-    <div className="bg-red-200 flex justify-center items-center w-full max-w-md m-auto h-36">
-      <p>Payment Cancel</p>
-    </div>
-  )
-}
+    <div className="flex gap-2 flex-col justify-center items-center  h-[70vh]">
+      <img width={100} src="https://res.cloudinary.com/dtmp7op6k/image/upload/v1687454414/vecteezy_hand-painted-rejected-sign_22000069_665_h4fn6f.png" />
+      <h1 className="font-medium text-xl">Your Payment is Failed...!!!</h1>
 
-export default Cancel
+      <p className="text-mainclr">Try Again...</p>
+      <Link to={"/"}>
+        <button className="bg-mainclr text-white rounded-full px-4 py-2">
+          Back to home
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Cancel;
--- Frontend/src/pages/Success.jsx
+++ Frontend/src/pages/Success.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Success = () => {
   return (
-    <div className="bg-green-200 flex justify-center items-center w-full max-w-md m-auto h-36">
-      <p>Payment Successfull</p>
+    <div className="flex gap-2 flex-col justify-center items-center  h-[70vh]">
+      <img
+        width={100}
+        src="https://res.cloudinary.com/dtmp7op6k/image/upload/v1687454555/vecteezy_green-tick-checkbox-illustration-isolated_10829962_2_ragbmh.png"
+      />
+      <h1 className="font-medium text-xl">Your Payment is Completed...!!!</h1>
+
+      <p className="text-mainclr">Order Again...</p>
+      <Link to={"/"}>
+        <button className="bg-mainclr text-white rounded-full px-4 py-2">
+          Back to home
+        </button>
+      </Link>
     </div>
   );
 };
