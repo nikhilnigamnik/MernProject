2efa26f56837e4da8b86b4adfc39df7527f94098
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-06-22 22:27:30 +0530

    update route cancel

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,24 +5,22 @@ import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
 import Login from "./pages/Login.jsx";
 import Success from "./pages/Success.jsx";
-import Cancle from "./pages/Cancle.jsx";
+import Cancel from "./pages/Cancel.jsx";
 import Newproduct from "./components/Admin/NewProduct.jsx";
 import Signup from "./pages/Signup.jsx";
 import Cart from "./pages/Cart.jsx";
 import About from "./pages/About.jsx";
 import "./index.css";
 import {
-  BrowserRouter,
-  Route,
   RouterProvider,
   createBrowserRouter,
-  createRoutesFromElements,
 } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/index.js";
 import Menu from "./pages/Menu.jsx";
 import Admin from "./components/Admin/Admin.jsx";
 import UserDetails from "./components/Admin/UserDetails.jsx";
+import Cancel from "./pages/Cancel.jsx";
 
 const router = createBrowserRouter([
   {
@@ -67,8 +65,8 @@ const router = createBrowserRouter([
         element: <Success />,
       },
       {
-        path: "/cancle",
-        element: <Cancle />,
+        path: "/cancel",
+        element: <Cancel />,
       },
       {
         path: "/admin/*",
--- Frontend/src/pages/Cancel.jsx
+++ Frontend/src/pages/Cancel.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Cancle = () => {
+const Cancel = () => {
   return (
     <div className="bg-red-200 flex justify-center items-center w-full max-w-md m-auto h-36">
       <p>Payment Cancel</p>
@@ -8,4 +8,4 @@ const Cancle = () => {
   )
 }
 
-export default Cancle
+export default Cancel
