535d2eebb0e689648be4efebd33f66fe2782969b
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-07-03 19:19:03 +0530

    image added in login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -30,16 +30,13 @@ export default function Login() {
     e.preventDefault();
     const { email, password } = data;
     if (email && password) {
-      const fetchData = await fetch(
-        `${baseURL}/login`,
-        {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
+      const fetchData = await fetch(`${baseURL}/login`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
       const dataRes = await fetchData.json();
 
       toast(userData.email + dataRes.message);
@@ -53,62 +50,60 @@ export default function Login() {
   };
 
   return (
-    <div className="flex h-[70vh]  justify-center items-center">
+    <div className="grid md:grid-cols-2 items-center justify-center">
       <div>
+        <img src="https://res.cloudinary.com/dtmp7op6k/image/upload/v1688391481/My_project_1_vjr1cx.png" />
+      </div>
+      <form className="sha2 rounded-xl p-8" onSubmit={handleSubmit}>
         <h2 className="text-4xl text-center  capitalize font-bold">Login</h2>
         <h1 className="capitalize my-4 text-center  text-normal font-semibold">
           Enter your details to login
         </h1>
-        <form
-          className="sha2 rounded-xl shadow-sm w-80 p-8 max-w-screen-lg sm:w-96"
-          onSubmit={handleSubmit}
-        >
-          <div className=" flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
-              <label className="capitalize text-normal">Email</label>
-              <input
-                size="lg"
-                id="email"
-                name="email"
-                className="border p-1 rounded-lg"
-                label="Email"
-                value={data.email}
-                onChange={handleOnChange}
-              />
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <label className="capitalize text-normal">Password</label>
-              <input
-                className="border p-1 rounded-lg"
-                type="password"
-                id="password"
-                name="password"
-                size="lg"
-                label="Password"
-                value={data.password}
-                onChange={handleOnChange}
-              />
-            </div>
+        <div className=" flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <label className="capitalize text-normal">Email</label>
+            <input
+              size="lg"
+              id="email"
+              name="email"
+              className="border p-1 rounded-lg"
+              label="Email"
+              value={data.email}
+              onChange={handleOnChange}
+            />
           </div>
 
-          <button
-            type="submit"
-            className=" bg-mainclr w-full my-4 rounded-full px-4 py-2 text-white"
+          <div className="flex flex-col gap-2">
+            <label className="capitalize text-normal">Password</label>
+            <input
+              className="border p-1 rounded-lg"
+              type="password"
+              id="password"
+              name="password"
+              size="lg"
+              label="Password"
+              value={data.password}
+              onChange={handleOnChange}
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className=" bg-mainclr w-full my-4 rounded-full px-4 py-2 text-white"
+        >
+          Login
+        </button>
+        <h1>
+          Do not have an account?{" "}
+          <Link
+            to="/signup"
+            className="font-medium text-red-500 transition-colors hover:text-red-700"
           >
-            Login
-          </button>
-          <h1>
-            Do not have an account?{" "}
-            <Link
-              to="/signup"
-              className="font-medium text-red-500 transition-colors hover:text-red-700"
-            >
-              Sign up
-            </Link>
-          </h1>
-        </form>
-      </div>
+            Sign up
+          </Link>
+        </h1>
+      </form>
     </div>
   );
 }
