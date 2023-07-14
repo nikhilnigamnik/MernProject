1bc15455f0022828e638ca4e2b4cddeb59de7450
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-07-14 22:44:50 +0530

    update link

--- Frontend/src/components/BannerSecond.jsx
+++ Frontend/src/components/BannerSecond.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BannerSecond = () => {
   return (
@@ -6,10 +7,15 @@ const BannerSecond = () => {
       <h1 className="text-center text-3xl font-bold">
         Best Food In Town Awaits You
       </h1>
-      <button className="bg-gradient-to-r text-center from-[#d5294d] to-[#B6035C]  px-4 py-2 rounded-full text-white">
-        Explore Our Food{" "}
-      </button>
-      <img className="rounded-xl" src="https://res.cloudinary.com/dtmp7op6k/image/upload/v1688240984/pexels-ella-olsson-1334131_qepamm.jpg" />
+      <Link to={"product"}>
+        <button className="bg-gradient-to-r text-center from-[#d5294d] to-[#B6035C]  px-4 py-2 rounded-full text-white">
+          Explore Our Food{" "}
+        </button>
+      </Link>
+      <img
+        className="rounded-xl"
+        src="https://res.cloudinary.com/dtmp7op6k/image/upload/v1688240984/pexels-ella-olsson-1334131_qepamm.jpg"
+      />
     </div>
   );
 };
